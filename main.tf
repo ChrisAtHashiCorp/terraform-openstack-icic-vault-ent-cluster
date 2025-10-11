60789d9abb89ad0a778701a5d75038ee5e0cd810
Chris Polansky <cpolansky@hashicorp.com>
2025-10-11 04:16:19 +0000

    Adding auto-unseal - fixing tls

--- main.tf
+++ main.tf
@@ -127,8 +127,8 @@ resource "ssh_resource" "vault-unseal" {
   password = var.ssh_conn.password
 
   commands = [
-    "vault operator unseal ${local.vault_init_res.unseal_keys_b64[0]}",
-    "vault operator unseal ${local.vault_init_res.unseal_keys_b64[1]}",
-    "vault operator unseal ${local.vault_init_res.unseal_keys_b64[2]}"
+    "vault operator unseal -tls-skip-verify ${local.vault_init_res.unseal_keys_b64[0]}",
+    "vault operator unseal -tls-skip-verify ${local.vault_init_res.unseal_keys_b64[1]}",
+    "vault operator unseal -tls-skip-verify ${local.vault_init_res.unseal_keys_b64[2]}",
   ]
 }
