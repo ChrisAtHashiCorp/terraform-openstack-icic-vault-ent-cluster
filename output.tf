d16ac59b19446ce2eb910571b295eb6c5bae9611
Chris Polansky <cpolansky@hashicorp.com>
2025-11-10 21:03:32 +0000

    Adding CA cert output

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "fqdns" {
   value = local.fqdns
 }
 
+output "ca_cert" {
+  value = tls_self_signed_cert.dev_ca_cert
+}
+
 output "unseal_keys" {
   value = local.vault_init_res.unseal_keys_b64
 }
