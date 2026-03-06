a02e9250572a031018905f70e730eabab0e1923d
Chris Polansky <cpolansky@hashicorp.com>
2026-03-06 05:22:09 +0000

    Adding Vault cluster record

--- main.tf
+++ main.tf
@@ -24,6 +24,15 @@ resource "aws_route53_record" "fqdns" {
   records = [openstack_compute_instance_v2.vault-nodes[each.key].access_ip_v4]
 }
 
+resource "aws_route53_record" "cluster" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "vaultclstr-${random_id.cluster_id}.${var.domain}"
+  type    = "A"
+  ttl     = 30
+  records = [openstack_compute_instance_v2.vault-nodes[*].access_ip_v4]
+}
+  
+
 # Create TLS certificates for the servers 
 
 # Generate a private key for the CA
